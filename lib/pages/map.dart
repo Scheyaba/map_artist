47f002ccbdeddd97d326d3842ede9ecebd6dde61
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-04-26 02:46:04 +0900

    show now location with bug

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Map extends StatefulWidget {
   const Map({super.key});
@@ -10,10 +11,21 @@ class Map extends StatefulWidget {
 
 class _MapState extends State<Map> {
   late GoogleMapController mapController;
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(37.4213114, -122.084869);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future(() async { // ToDO: パーミッションを選択しても一度画面を更新しないとGPSが表示されない
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        await Geolocator.requestPermission();
+      }
+    });
   }
 
   @override
@@ -27,9 +39,11 @@ class _MapState extends State<Map> {
       ),
       body: GoogleMap(
           onMapCreated: _onMapCreated,
+          zoomControlsEnabled: false,
+          myLocationEnabled: true,
           initialCameraPosition: CameraPosition(
             target: _center,
-            zoom: 11.0,
+            zoom: 14.0,
           ),
         ),
       );

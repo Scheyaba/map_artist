626c25b4d845902a7070853482a05a97b406dd43
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-04-27 05:47:01 +0900

    refactor code

--- lib/pages/components/map_widget.dart
+++ lib/pages/components/map_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:async';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -12,31 +12,36 @@ class MapWidget extends StatefulWidget {
 }
 
 class _MapWidgetState extends State<MapWidget> {
-  late GoogleMapController mapController;
-  final LatLng _center = const LatLng(35.6816, 139.7655);
+  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
   late LocationPermission permissionCopy;
+  late Position _position;
+  final LatLng _center = const LatLng(35.6816, 139.7655);
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
-  Future<void> _goToNowlocation() async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  Future<void> _goToCurrentLocation() async {
+    final GoogleMapController mapController = await _controller.future;
     mapController.animateCamera(
       CameraUpdate.newCameraPosition(
         CameraPosition(
-          bearing: position.heading,
-          target: LatLng(position.latitude, position.longitude),
+          bearing: _position.heading,
+          target: LatLng(_position.latitude, _position.longitude),
           zoom: 14
         )
       )
     );
   }
 
+  Future<void> _getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _position = position;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     permissionCopy = widget.permission;
+    _getCurrentLocation();
   }
 
   @override
@@ -44,7 +49,9 @@ class _MapWidgetState extends State<MapWidget> {
     if (permissionCopy == LocationPermission.whileInUse || permissionCopy == LocationPermission.always) {
       return Scaffold(
         body: GoogleMap(
-          onMapCreated: _onMapCreated,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
           zoomControlsEnabled: false,
           myLocationEnabled: true,
           myLocationButtonEnabled:false,
@@ -54,7 +61,7 @@ class _MapWidgetState extends State<MapWidget> {
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: _goToNowlocation,
+          onPressed: _goToCurrentLocation,
           backgroundColor: Colors.white,
           child: Icon(
             Icons.my_location,
@@ -66,7 +73,9 @@ class _MapWidgetState extends State<MapWidget> {
     else{
       return Scaffold(
         body: GoogleMap(
-          onMapCreated: _onMapCreated,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
           zoomControlsEnabled: false,
           myLocationEnabled: false,
           initialCameraPosition: CameraPosition(

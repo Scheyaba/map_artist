bcee9027c30a4b1c72781fbc35f2b2ed557d278e
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-04-29 13:03:44 +0900

    ready to use firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'dart:ui' as ui;
+import 'dart:convert';
+import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:map_artist/data/database.dart';
 import 'package:map_artist/providers/database_provider.dart';
 import 'package:map_artist/providers/app_theme_provider.dart';
@@ -13,7 +20,11 @@ import 'package:map_artist/theme/theme.dart';
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   final dbHelper = DbHelper();
-  await dbHelper.initialize();  
+  await dbHelper.initialize();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   
   runApp(ProviderScope(
     overrides: [
